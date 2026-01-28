bedf56c0037b0293aad988f033a465b8d1fca8c7
Sahil Shakya <sahilshakya622@gmail.com>
2026-01-28 16:15:33 +0530

    Add blob trigger function

--- function-app/function_app.py
+++ function-app/function_app.py
@@ -1,11 +1,13 @@
+import logging
 import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.function_name(name="healthcheck")
-@app.route(route="health", auth_level=func.AuthLevel.ANONYMOUS)
-def health(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(
-        "Function App is working via GitHub Actions",
-        status_code=200
-    )
+@app.blob_trigger(
+    arg_name="myblob",
+    path="input-files/{name}",
+    connection="AzureWebJobsStorage"
+)
+def process_blob(myblob: func.InputStream):
+    logging.info(f"Blob name: {myblob.name}")
+    logging.info(f"Blob size: {myblob.length} bytes")
